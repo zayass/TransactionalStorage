d15fd46d9407955ee21c89a2f31a4dfdcb7e22b8
Anton Pogonets <zayass@gmail.com>
2024-08-01 17:14:26 +0200

    Fix lambda recomposition

--- build-logic/convention/src/main/kotlin/org/zayass/buildlogic/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/org/zayass/buildlogic/AndroidCompose.kt
@@ -2,7 +2,11 @@ package org.zayass.buildlogic
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 /**
  * Configure Compose-specific options
@@ -31,5 +35,24 @@ internal fun Project.configureAndroidCompose(
                 isIncludeAndroidResources = true
             }
         }
+    }
+
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+        fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
+        fun Provider<*>.relativeToRootProject(dir: String) = flatMap {
+            rootProject.layout.buildDirectory.dir(projectDir.toRelativeString(rootDir))
+        }.map { it.dir(dir) }
+
+        project.providers.gradleProperty("enableComposeCompilerMetrics").onlyIfTrue()
+            .relativeToRootProject("compose-metrics")
+            .let(metricsDestination::set)
+
+        project.providers.gradleProperty("enableComposeCompilerReports").onlyIfTrue()
+            .relativeToRootProject("compose-reports")
+            .let(reportsDestination::set)
+
+        stabilityConfigurationFile = rootProject.layout.projectDirectory.file("compose_compiler_config.conf")
+
+        enableStrongSkippingMode = true
     }
 }
